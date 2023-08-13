1da783e1ef6b561435b320feb5c9c099ec300136
eugenestar5 <eugene.bonsu@gmail.com>
2023-08-13 17:22:41 -0600

    Adding modified files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,13 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E.'
+      }),
 
+      
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Just Another Text Editor',
+        background_color: '#ffffff',
+        fingerprints: false,
+        publicPath: '.',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+    ],
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
 };
+
